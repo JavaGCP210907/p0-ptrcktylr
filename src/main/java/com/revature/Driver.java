984962c1e89ee3a016e131c130057f8d4500d36a
ptrcktylr <ptrcktylr@icloud.com>
2021-09-21 23:59:09 -0400

    removed unused imports

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -3,10 +3,7 @@ package com.revature;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import com.revature.dao.AddressDao;
-import com.revature.dao.LoginDao;
 import com.revature.models.Menu;
-import com.revature.models.Session;
 import com.revature.utils.ConnectionUtil;
 
 public class Driver {
